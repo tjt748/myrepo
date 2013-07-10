47899382c1eb531fb3f34987b019477a5bed3c8c
tjt748 <tjt748@yahoo.com>
2013-07-10 19:04:51 +0000

    Replaced with old web.js

--- web.js
+++ web.js
@@ -3,12 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-
-  var fs = require('fs');
-
-  var buffer = new Buffer(fs.readFileSync("index.html"), 'utf8');
-
-  response.send(buffer); 
+  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
